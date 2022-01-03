0b328de328e9f71127ed0039e17adff948dfdba3
YaminipriyaDayanithi <96522128+YaminipriyaDayanithi@users.noreply.github.com>
2022-01-03 09:23:20 +0530

    Add files via upload

---
+++ CaseStudies/Assignment5/Comparator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment5
+{
+    class NoOfReadsComparator : IComparer<Story>
+    {
+        public int Compare(Story x, Story y)
+        {
+            return x.NoOfReads.CompareTo(y.NoOfReads);
+        }
+    }
+}
---
+++ CaseStudies/Assignment5/Story.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment5
+{
+    class Story : IComparable
+    {
+        private string name, authorName, genre;
+        private int noOfChapters, noOfLikes, noOfReads;
+
+        public Story(string name, string authorName, string genre, int noOfChapters, int noOfLikes, int noOfReads)
+        {
+            this.Name = name;
+            this.AuthorName = authorName;
+            this.Genre = genre;
+            this.NoOfChapters = noOfChapters;
+            this.NoOfLikes = noOfLikes;
+            this.NoOfReads = noOfReads;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public string AuthorName { get => authorName; set => authorName = value; }
+        public string Genre { get => genre; set => genre = value; }
+        public int NoOfChapters { get => noOfChapters; set => noOfChapters = value; }
+        public int NoOfLikes { get => noOfLikes; set => noOfLikes = value; }
+        public int NoOfReads { get => noOfReads; set => noOfReads = value; }
+
+
+        public int CompareTo(object obj)
+        {
+            Story story = (Story)obj;
+            return this.Name.CompareTo(story.Name);
+        }
+
+        public override string ToString()
+        {
+            return string.Format(this.Name + " " + " " + this.AuthorName + " " + this.Genre
+                + " " + this.NoOfChapters + " " + this.NoOfLikes + " " + this.NoOfReads);
+        }
+
+    }
+}
